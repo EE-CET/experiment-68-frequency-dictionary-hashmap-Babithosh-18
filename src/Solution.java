a736852efba6de504942c842b2deda47b2b3468a
Babithosh <ellu250071@cet.ac.in>
2026-02-28 18:27:24 +0530

    added hashmap code

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -16,8 +16,8 @@ public class Solution {
         
         // TODO: Iterate through the map and print the unique words and their counts
         // Format: "word: count"
-
-	 String line = scanner.nextLine();
+	
+	String line = scanner.nextLine();
         String[] words = line.split("\\s+");
 
         LinkedHashMap<String, Integer> freqMap = new LinkedHashMap<>();
